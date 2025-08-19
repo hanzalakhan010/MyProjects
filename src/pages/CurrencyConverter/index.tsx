0fe641a40e5de83bf702da0b9286642e413b242c
Hanzala Khan <pythondeveloper360@gmail.com>
2025-08-19 10:07:27 +0500

    Comment out CurrencyConverter component code for future reference

--- src/pages/CurrencyConverter/index.tsx
+++ src/pages/CurrencyConverter/index.tsx
@@ -1,33 +1,33 @@
-const CurrencyConverter = () => {
-    function convert() {
-        let rates = JSON.parse(localStorage.getItem('rates') || '')
-        let to = document.getElementById('to').value
-        let from = document.getElementById('from').value
-        let currency = document.getElementById('currency').value
-        let convereted_curr = ((rates['USD'] / rates[from]) * rates[to]) * currency
-        document.getElementById('currency_converted').value = convereted_curr.toFixed(3)
-    }
-    return (<>
-        <h1>Currency Converter</h1>
-        <div id='main'>
-            <div id='input_currency'>
-                <select id='from'>
-                    <option>Select currency</option>
-                </select>
-                <input type="number" id='currency' />
-            </div>
-            <div id='ouput_currency'>
-                <select id='to'>
-                    <option>Select currency</option>
-                </select>
-                <input type="number" id='currency_converted' disabled />
-            </div>
-            <div></div>
-            <button onClick={convert}>Convert</button>
-        </div>
-    </>)
+// const CurrencyConverter = () => {
+//     function convert() {
+//         let rates = JSON.parse(localStorage.getItem('rates') || '')
+//         let to = document.getElementById('to').value
+//         let from = document.getElementById('from').value
+//         let currency = document.getElementById('currency').value
+//         let convereted_curr = ((rates['USD'] / rates[from]) * rates[to]) * currency
+//         document.getElementById('currency_converted').value = convereted_curr.toFixed(3)
+//     }
+//     return (<>
+//         <h1>Currency Converter</h1>
+//         <div id='main'>
+//             <div id='input_currency'>
+//                 <select id='from'>
+//                     <option>Select currency</option>
+//                 </select>
+//                 <input type="number" id='currency' />
+//             </div>
+//             <div id='ouput_currency'>
+//                 <select id='to'>
+//                     <option>Select currency</option>
+//                 </select>
+//                 <input type="number" id='currency_converted' disabled />
+//             </div>
+//             <div></div>
+//             <button onClick={convert}>Convert</button>
+//         </div>
+//     </>)
 
-}
+// }
 
 
-export default CurrencyConverter;
+// export default CurrencyConverter;
